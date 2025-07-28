3130fb4f69e66a2407aae646e9e59b24bfe295d5
BhanuPrakash6640 <bhanuprakash12030@gmail.com>
2025-07-28 10:22:21 +0530

    All skill programs

---
+++ Skill/Wrapperclass.java
@@ -0,0 +1,12 @@
+
+// Topic: Type Conversion Using Wrapper Classes
+
+public class Wrapperclass {
+    public static void main(String[] args) {
+        String str = "123";
+        int num = Integer.parseInt(str); 
+
+        System.out.printf("Original string: %s\n", str);
+        System.out.printf("Converted int: %d\n", num);
+    }
+}
---
+++ Skill/typeconversion.java
@@ -0,0 +1,13 @@
+
+// Topic: Type Conversion
+//From int to float
+
+public class typeconversion {
+    public static void main(String[] args) {
+        int num = 100;
+        float f = num; 
+
+        System.out.printf("Original int value: %d\n", num);
+        System.out.printf("After widening to float: %.2f\n", f);
+    }
+}
