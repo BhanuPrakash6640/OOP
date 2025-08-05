e3ff4782861693134efdb60fba243d6bbd60679d
BhanuPrakash6640 <bhanuprakash12030@gmail.com>
2025-08-05 10:36:06 +0530

    Practical 2

---
+++ Practicaltask2/StringImmutability.java
@@ -0,0 +1,12 @@
+package Practicaltask2;
+
+public class StringImmutability {
+	public static void main(String[]args) {
+		String message= "Hello";
+		System.out.printf("original message:%d\n", message);
+		
+		message.concat("world");
+		
+	}
+
+}
