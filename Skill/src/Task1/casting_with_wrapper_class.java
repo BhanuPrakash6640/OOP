bfa86ba057a4bb4600921419d43c85f15e2d5427
BhanuPrakash6640 <bhanuprakash12030@gmail.com>
2025-08-05 11:07:48 +0530

    Skill

---
+++ Skill/src/Task1/casting_with_wrapper_class.java
@@ -0,0 +1,17 @@
+
+package Task1;
+
+public class casting_with_wrapper_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Integer intObj = 50;
+        Double doubleObj = (double) intObj;
+
+        System.out.println("Wrapper Casting:");
+        System.out.println("Integer object: " + intObj);
+        System.out.println("Casted to Double object: " + doubleObj);
+    }
+	}
+
+
